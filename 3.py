30cd2372bae6d42c758d9834405b19638919cc1b
Hassnmrx-001 <109261131+Hassnmrx-001@users.noreply.github.com>
2022-07-14 10:40:27 +0100

    Add files via upload

---
+++ 3.py
@@ -0,0 +1,17 @@
+from turtle import *
+import time
+color('green')
+bgcolor('black')
+speed(25)
+right(45)
+for n in range(150):
+    if 7 < n < 62 :
+        left(5)
+    if 80 < n < 133 :
+        right(5)
+    circle(30)
+    if n < 80 :
+        forward (10)
+    else:
+        forward(5)
+time.sleep(5)
